b2d1c00447b683a83d1a5f1c08716b5cc25044ea
eldeni <eldeniyenden@gmail.com>
2018-12-07 19:12:47 +0900

    feat: define `eject()`, which creates html in string
    - eject() generates static file in the file system.
    
    Fixes https://github.com/nodekit-org/express-isomorphic/issues/1

--- packages/express-isomorphic/example/example.ts
+++ packages/express-isomorphic/example/example.ts
@@ -4,18 +4,22 @@ import ExpressIsomorphic from '../lib';
 import * as paths from './paths';
 import makeHtml from './src/makeHtml';
 
-const { localServer, productionServer } = ExpressIsomorphic.create({
+const { localServer, productionServer, eject } = ExpressIsomorphic.create({
   bundlePath: paths.distPublicBundle,
+  ejectPath: paths.ejectPath,
   makeHtml,
   publicPath: paths.dist,
   serverDistPath: paths.dist,
-  universalAppPath: paths.dist,
+  universalAppPath: paths.universalApp,
   webpackConfigClientLocalPath: paths.webpackConfigClientLocalWeb,
   webpackConfigUniversalLocalPath: paths.webpackConfigUniversalLocal,
 });
 
 const port = 5234
-const httpServer = http.createServer(localServer().app);
-httpServer.listen(port, () => {
-  console.log(`Listening on ${port}`);
-});
+
+// const httpServer = http.createServer(localServer().app);
+// httpServer.listen(port, () => {
+//   console.log(`Listening on ${port}`);
+// });
+
+eject();
--- packages/express-isomorphic/lib/index.js
+++ packages/express-isomorphic/lib/index.js
@@ -5,8 +5,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const localServer_1 = __importDefault(require("./localServer"));
 const productionServer_1 = __importDefault(require("./productionServer"));
-const create = function ({ bundlePath, makeHtml, publicPath, serverDistPath, universalAppPath, webpackConfigClientLocalPath, webpackConfigUniversalLocalPath, webpackStats = defaultWebpackStats, }) {
+const eject_1 = __importDefault(require("./eject"));
+const create = function ({ bundlePath, ejectPath = '', makeHtml, publicPath, serverDistPath, universalAppPath, webpackConfigClientLocalPath, webpackConfigUniversalLocalPath, webpackStats = defaultWebpackStats, }) {
     return {
+        eject: () => eject_1.default({
+            bundlePath,
+            ejectPath,
+            makeHtml,
+            publicPath,
+            universalAppPath,
+        }),
         localServer: () => localServer_1.default({
             makeHtml,
             publicPath,
@@ -34,6 +42,7 @@ const defaultWebpackStats = {
     color: true,
     entrypoints: true,
 };
+exports.webpackStats = defaultWebpackStats;
 exports.default = ExpressIsomorphic;
 var serverUtils_1 = require("./utils/serverUtils");
 exports.attachAssets = serverUtils_1.attachAssets;
