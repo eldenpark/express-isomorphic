4d06a6a68626b03f92c5ac0fc62a94b280ac730c
eldeni <eldeniyenden@gmail.com>
2018-12-30 20:29:27 +0900

    hotfix: deactivate eject() from example

--- packages/express-isomorphic/example/paths.js
+++ packages/express-isomorphic/example/paths.js
@@ -6,7 +6,7 @@ const EXAMPLE_PATH = path.resolve(ROOT_PATH, 'example');
 module.exports = {
   dist: path.resolve(EXAMPLE_PATH, 'dist'),
   distPublicBundle: path.resolve(EXAMPLE_PATH, 'dist/bundle'),
-  ejectPath: path.resolve(EXAMPLE_PATH, 'dist/index.html'),
+  distEject: path.resolve(EXAMPLE_PATH, 'dist/eject'),
   src: path.resolve(EXAMPLE_PATH, 'src'),
   universalApp: path.resolve(EXAMPLE_PATH, 'dist/universal.local.rootContainer'),
   webpackConfigClientLocalWeb: path.resolve(EXAMPLE_PATH, 'webpack/webpack.config.client.local.web'),
