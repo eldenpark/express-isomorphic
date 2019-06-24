ed98f4f1e9cdaa881b5b046743569877cb8d76f2
Elden <eldeniyenden@gmail.com>
2019-06-24 17:39:18 +0900

    chore: add type definition file

---
+++ packages/express-isomorphic-react/lib/index.d.ts
@@ -0,0 +1,39 @@
+import { Application } from 'express';
+import { ServerState } from '@nodekit/express-isomorphic';
+export declare function withReactLocal<State extends ReactServerState>({ serverState, webpackConfig, webpackStats, }: WithReactLocalArgs<State>): (app: Application) => Application;
+export declare function withReactProd<State extends ReactServerState>({ serverState, webpackBuild, webpackConfig, }: WithReactProdArgs<State>): (app: Application) => Application;
+export interface WebpackStats {
+    chunks: boolean;
+    entrypoints: boolean;
+    [key: string]: boolean;
+}
+export interface WebpackConfig {
+    output: {
+        [key: string]: any;
+    };
+    [key: string]: any;
+}
+export interface ReactServerState {
+    buildHash: string;
+}
+interface WebpackBuild {
+    assets: any[];
+    builtAt: number;
+    entrypoints: {
+        [key: string]: {
+            assets: string[];
+        };
+    };
+    errors: any[];
+}
+interface WithReactLocalArgs<State extends ReactServerState> {
+    serverState: ServerState<State>;
+    webpackConfig: WebpackConfig;
+    webpackStats: WebpackStats;
+}
+interface WithReactProdArgs<State extends ReactServerState> {
+    serverState: ServerState<State>;
+    webpackBuild: WebpackBuild;
+    webpackConfig: WebpackConfig;
+}
+export {};
