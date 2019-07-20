e42a9fff41c58e12d8c4d02b75750b99d3af559c
Elden <eldeniyenden@gmail.com>
2019-07-21 00:26:01 +0900

    chore: export Isomorphic object

--- packages/express-isomorphic-react/src/universal/index.tsx
+++ packages/express-isomorphic-react/src/universal/index.tsx
@@ -1,11 +1,13 @@
 import {
   createIsomorphic,
+  Isomorphic,
   IsomorphicProvider,
 } from './Isomorphic';
 import useFetch from './useFetch';
 
 export {
   createIsomorphic,
+  Isomorphic,
   IsomorphicProvider,
   useFetch,
 };
