781e8c0eada7f45f28bacf7e0a9d53d7caf1fb55
Elden <eldeniyenden@gmail.com>
2019-07-30 13:38:44 +0900

    chore: add `process-launch` and revamp launcher

--- scripts/launcher.js
+++ scripts/launcher.js
@@ -1,42 +1,33 @@
 const { argv } = require('yargs');
-const childProcess = require('child_process');
-const fs = require('fs');
+const { createLaunch, proc } = require('process-launch');
 const { logger } = require('jege/server');
-const path = require('path');
-
-const { requireNonNull } = require('./scriptUtils');
 
 const log = logger('[monorepo-express-isomorphic]');
+
+const processDefinitions = {
+  'example-react': proc(
+    'node',
+    [
+      './scripts/launch.js',
+      ...argv._,
+    ],
+    {
+      cwd: `./packages/example-react`,
+      stdio: 'inherit',
+    },
+  ),
+};
 
 function launcher() {
   try {
     log('launcher(): argv: %j', argv);
 
-    requireNonNull(argv.p, 'You should provide "-p" with package name');
+    const launch = createLaunch({
+      processDefinitions,
+    });
 
-    const cwdForLaunch = path.resolve(__dirname, '../packages', argv.p);
-    const launchFileRelativePath = './scripts/launch.js';
-    const launchFilePath = path.resolve(cwdForLaunch, launchFileRelativePath);
-    log('launcher(): launchFilePath: %s, cwdForLaunch: %s', launchFilePath, cwdForLaunch);
-
-    if (!fs.existsSync(launchFilePath)) {
-      throw new Error('launch file does not exist');
-    }
-
-    const launch = childProcess.spawn(
-      'node',
-      [
-        './scripts/launch.js',
-        ...argv._,
-      ],
-      {
-        cwd: `./packages/${argv.p}`,
-        stdio: 'inherit',
-      },
-    );
-
-    launch.on('error', (err) => {
-      log('launcher(): error in process: $s, error: $o', argv.p, err);
+    launch({
+      process: argv.process,
     });
   } catch (err) {
     log('launcher(): Error reading file', err);
